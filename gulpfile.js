a84dde310e563edcf6132805ef6e787da55f9d8a
teamrun <chenllos@163.com>
2014-11-28 14:48:41 +0800

    用上less 调整样式

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,72 @@
 var gulp = require('gulp');
 
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var liveReload = require('gulp-livereload');
+var rename = require('gulp-rename');
+
+var less = require('gulp-less');
+
 var gbro = require('gulp-browserify');
 var reactify = require('reactify');
 var envify = require('envify');
 
-var gutil = require('gulp-util');
-var plumber = require('gulp-plumber');
-var rename = require('gulp-rename');
-
-var liveReload = require('gulp-livereload');
 
 function errHandler(err){
     gutil.beep();
     gutil.log(err);
 }
 
-gulp.task('bundle', function(){
-    gulp.src('lib/app.js')
+
+var productTasks = [undefined, 'default'];
+var proEnv = false;
+
+var taskName = (process.argv[0] === 'node')? process.argv[2] : process.argv[1];
+if( productTasks.indexOf(taskName) >= 0 ){
+    proEnv = true;
+}
+
+var main = {
+    less: ['layout/less/layout.less'],
+    js: ['lib/app.js']
+};
+
+gulp.task('less', function(){
+    gulp.src( main.less )
         .pipe( plumber({errorHandler: errHandler}) )
-        .pipe( gbro({
-            transform: [reactify, envify],
-                debug: true
-        }) )
-        .pipe( rename('bundle.js') )
-        .pipe(gulp.dest('lib/'))
+        .pipe( less({dumpLineNumbers: 'comments'}) )
+        .pipe( gulp.dest('layout/css') );
 });
+
+
+gulp.task('react', function(){
+    gulp.src( main.js )
+        .pipe(plumber({errorHandler: errHandler}))
+        .pipe(gbro({
+            transform: [reactify, envify],
+            debug: !proEnv
+        }))
+        .pipe(rename('bundle.js'))
+        .pipe(gulp.dest('./lib'));
+});
+
+
+
 
 gulp.task('watch', function(){
     liveReload.listen();
 
-    gulp.watch(['lib/**/*.js', '!lib/bundle.js'], ['bundle']);
+    var less2Watch = ['layout/less/**/*.less'];
+    var less2Reload = ['layout/css/layout*.css'];
+    gulp.watch(less2Watch,  ['less']);
+    gulp.watch(less2Reload, liveReload.changed);
 
-    gulp.watch(['lib/bundle.js', 'layout/layout.css'], liveReload.changed);
+    var js2Watch = ['./lib/**/*.js', '!./lib/bundle.js'];
+    var js2Reload = ['./lib/bundle.js'];
+    gulp.watch(js2Watch, ['react']);
+    gulp.watch(js2Reload, liveReload.changed);
+
 });
 
-gulp.task('default', ['bundle']);
-
-gulp.task('wd', ['bundle', 'watch']);
+gulp.task('default', ['less', 'react']);
+gulp.task('wd', ['less', 'react', 'watch']);
